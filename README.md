b406bb81660e1507e5fbdc18d42481dc8af7b977
고은별 <mgeraghty98@gmail.com>
2023-05-19 17:24:55 +1000

    Test TWRP Device tree (probably wont work)

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-hey
+# Android device tree for samsung SM-A326B (a32x)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ twrp_a32x.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from a32x device
+$(call inherit-product, device/samsung/a32x/device.mk)
+
+PRODUCT_DEVICE := a32x
+PRODUCT_NAME := omni_a32x
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A326B
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a32xxx-user 11 RP1A.200720.012 A326BXXU1AUB6 release-keys"
+
+BUILD_FINGERPRINT := samsung/a32xxx/a32x:11/RP1A.200720.012/A326BXXU1AUB6:user/release-keys
