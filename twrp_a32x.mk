628216045a0c9b2d47e60d89abbcb5b3140b597f
고은별 <mgeraghty98@gmail.com>
2023-05-19 21:26:09 +1000

    Working Device Tree for TWRP

--- twrp_a32x.mk
+++ twrp_a32x.mk
@@ -8,13 +8,18 @@
 # Inherit from those products. Most specific first.
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a32x device
 $(call inherit-product, device/samsung/a32x/device.mk)
+
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a32x/recovery/root,recovery/root)
 
 PRODUCT_DEVICE := a32x
 PRODUCT_NAME := twrp_a32x
